f646da5758dafd0e08811b3f408b3cba9d802c6e
Seven Du <shiweidu@outlook.com>
2017-07-26 16:44:09 +0800

    完善话题数据模型，增加头像支持。

--- src/Models/Topic.php
+++ src/Models/Topic.php
@@ -3,8 +3,57 @@
 namespace SlimKit\PlusQuestion\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Zhiyi\Plus\Models\Concerns\HasAvatar;
+use Zhiyi\Plus\Contracts\Model\ShouldAvatar as ShouldAvatarContract;
 
-class Topic extends Model
+class Topic extends Model implements ShouldAvatarContract
 {
-    // todo.
+    use HasAvatar;
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = ['created_at', 'updated_at'];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = ['avatar'];
+
+    /**
+     * Get topic avatar attribute.
+     *
+     * @return string|null
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function getAvatarAttribute()
+    {
+        return $this->avatar();
+    }
+
+    /**
+     * Get avatar trait.
+     *
+     * @return string|int
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function getAvatarKey()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Avatar prefix.
+     *
+     * @return string
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function getAvatarPrefix(): string
+    {
+        return 'question/topics';
+    }
 }
