ce4a96fb032a33602ae943d94305537453538fb3
Seven Du <shiweidu@outlook.com>
2017-07-26 12:21:51 +0800

    注册处理器。

--- src/PackageHandler.php
+++ src/PackageHandler.php
@@ -7,6 +7,24 @@ use Illuminate\Console\Command;
 class PackageHandler extends \Zhiyi\Plus\Support\PackageHandler
 {
     /**
+     * Publish public source handle.
+     *
+     * @param \Illuminate\Console\Command $command
+     * @return mixed
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function publishPublicHandle(Command $command)
+    {
+        $force = $command->confirm('Overwrite any existing files');
+
+        return $command->call('vendor:publish', [
+            '--provider' => \SlimKit\Component\PlusQuestion\ServiceProvider::class,
+            '--tag' => 'public',
+            '--force' => boolval($force)
+        ])
+    }
+
+    /**
      * The migrate handle.
      *
      * @param \Illuminate\Console\Command $command
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -3,4 +3,58 @@
 namespace SlimKit\Component\PlusQuestion;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
-{}
+{
+    public function register()
+    {
+        // register cntainer aliases
+        $this->registerContainerAliases();
+
+        // Register Plus package handlers.
+        $this->registerPackageHandlers();
+    }
+
+    /**
+     * Register container aliases.
+     *
+     * @return void
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    protected function registerContainerAliases()
+    {
+        $aliases = [
+            'plus-question:handler' => [
+                SlimKit\Component\PlusQuestion\PackageHandler::class,
+            ],
+        ];
+
+        foreach ($aliases as $key => $aliases) {
+            foreach ($aliases as $key => $alias) {
+                $this->app->alias($key, $alias);
+            }
+        }
+    }
+
+    /**
+     * Register Plus package handlers.
+     *
+     * @return void
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    protected function registerPackageHandlers()
+    {
+        $this->loadHandleFrom('question', 'plus-question:handler');
+    }
+
+    /**
+     * Register handler.
+     *
+     * @param string $name
+     * @param \Zhiyi\Plus\Support\PackageHandler|string $handler
+     * @return void
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    private function loadHandleFrom(string $name, $handler)
+    {
+        \Zhiyi\Plus\Support\PackageHandler::loadHandleFrom($name, $handler);
+    }
+}
