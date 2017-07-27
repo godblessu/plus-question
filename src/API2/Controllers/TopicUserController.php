b6671ad45873ca1119faa882ae122c90999879f1
Seven <shiweidu@outlook.com>
2017-07-27 02:11:46 +0000

    Apply fixes from StyleCI

--- src/API2/Controllers/TopicUserController.php
+++ src/API2/Controllers/TopicUserController.php
@@ -3,15 +3,15 @@
 namespace SlimKit\PlusQuestion\API2\Controllers;
 
 use Illuminate\Http\Request;
-use Zhiyi\Plus\Models\User as UserModel;
 use Illuminate\Contracts\Routing\ResponseFactory as ResponseFactoryContract;
 
 class TopicUserController extends Controller
 {
-    public function index (Request $request, ResponseFactoryContract $response)
+    public function index(Request $request, ResponseFactoryContract $response)
     {
         $user = $this->resolveUser($request->user());
 
-        var_dump($user);exit;
+        var_dump($user);
+        exit;
     }
 }
