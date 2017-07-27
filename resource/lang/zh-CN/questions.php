d4967a13a98d0ec06ada05e6e9af4a583ad54df6
Seven Du <shiweidu@outlook.com>
2017-07-27 16:55:55 +0800

    增加问题相关语言包。

---
+++ resource/lang/zh-CN/questions.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    
+    'attributes' => [
+        'subject' => '主题',
+        'topics' => '话题',
+        'anonymity' => '匿名',
+        'look' => '围观',
+    ],
+
+    'Attribute must end with a question mark' => '主题必须以问号结尾',
+    'Insufficient balance' => '余额不足',
+];
