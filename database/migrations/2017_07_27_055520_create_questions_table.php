a3568a6b216bd1be5fcb1eb86e20b869eedeb8bd
Seven Du <shiweidu@outlook.com>
2017-07-28 11:16:11 +0800

    修复问题数据表迁移bug。

--- database/migrations/2017_07_27_055520_create_questions_table.php
+++ database/migrations/2017_07_27_055520_create_questions_table.php
@@ -22,7 +22,7 @@ class CreateQuestionsTable extends Migration
             $table->integer('amount')->unsigned()->nullable()->default(0)->comment('悬赏总额，如果为 0，则不走账务流程。');
             $table->tinyInteger('automaticity')->unsigned()->nullable()->default(0)->comment('回答自动入帐');
             $table->tinyInteger('look')->unsigned()->nullable()->default(0)->comment('是否围观');
-            $table->tinyInteger('excellent')->unsigned()->nullable()->default('是否是杰出的，精华。');
+            $table->tinyInteger('excellent')->unsigned()->nullable()->default(0)->comment('是否是杰出的，精华。');
             $table->integer('comments_count')->nullable()->default(0)->comment('问题评论统计');
             $table->integer('answers_count')->nullable()->default(0)->comment('答案数量统计');
             $table->integer('watchers_count')->nullable()->default(0)->comment('关注者统计');
