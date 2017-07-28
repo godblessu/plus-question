0a646fc2e3c5676f3626892eac91451340eea365
Seven Du <shiweidu@outlook.com>
2017-07-28 16:39:28 +0800

    增加回答数据表。

---
+++ database/migrations/2017_07_28_070552_create_answers_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('question_id')->unsigned()->comment('所属问题');
+            $table->integer('user_id')->unsigned()->comment('用户');
+            $table->text('body')->nullable()->default(null)->comment('回答内容');
+            $table->tinyInteger('anonymity')->unsigned()->nullable()->default(0)->comment('是否作者匿名');
+            $table->tinyInteger('adoption')->nullable()->default(0)->comment('是否被采纳');
+            $table->tinyInteger('invited')->nullable()->default(0)->comment('是否是被邀请回答');
+            $table->integer('comments_count')->nullable()->default(0)->comment('问题评论统计');
+            $table->integer('rewards_amount')->unsigned()->nullable()->default(0)->comment('打赏总额');
+            $table->integer('likes_count')->nullable()->default(0)->comment('喜欢数量统计');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+}
