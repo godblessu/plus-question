85bc7e8a6456c22e1f979af0270275ed5d67e9c9
Seven Du <shiweidu@outlook.com>
2017-07-31 15:33:30 +0800

    答案增加问题关系。

--- src/Models/Answer.php
+++ src/Models/Answer.php
@@ -6,4 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
+    /**
+     * Has the answer for the question.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne|null
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function question()
+    {
+        return $this->hasOne(Question::class, 'id', 'question_id');
+    }
 }
