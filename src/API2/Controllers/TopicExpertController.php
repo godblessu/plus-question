94c46355c3ed15252f5d1dcdcc03796291f4c352
Seven Du <shiweidu@outlook.com>
2017-08-01 19:04:11 +0800

    完成话题专家列表接口。

--- src/API2/Controllers/TopicExpertController.php
+++ src/API2/Controllers/TopicExpertController.php
@@ -3,20 +3,39 @@
 namespace SlimKit\PlusQuestion\API2\Controllers;
 
 use Illuminate\Http\Request;
+use Zhiyi\Plus\Models\User as UserModel;
 use SlimKit\PlusQuestion\Models\Topic as TopicModel;
 use Illuminate\Contracts\Routing\ResponseFactory as ResponseFactoryContract;
 
 class TopicExpertController extends Controller
 {
+    /**
+     * Get all experts for the topics.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Illuminate\Contracts\Routing\ResponseFactory $response
+     * @param \SlimKit\PlusQuestion\Models\Topic $topic
+     * @return mixed
+     * @author Seven Du <shiweidu@outlook.com>
+     */
     public function index(Request $request, ResponseFactoryContract $response, TopicModel $topic)
     {
+        $userID = $request->user('api')->id ?? 0;
         $after = $request->query('after');
         $users = $topic->experts()
+            ->with('tags')
             ->when($after, function ($query) use ($after) {
                 return $query->where('id', '<', $after);
             })
+            ->limit(20)
+            ->orderBy('id', 'desc')
             ->get();
 
-        dd($users);
+        return $response->json($users->map(function (UserModel $user) use ($userID) {
+            $user->following = $user->hasFollwing($userID);
+            $user->follower = $user->hasFollower($userID);
+
+            return $user;
+        }))->setStatusCode(200);
     }
 }
