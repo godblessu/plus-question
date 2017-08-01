cda8986a95c1bcd94b1d4d20429ba99bf0b9d141
Seven Du <shiweidu@outlook.com>
2017-08-01 10:33:16 +0800

    增加回答用户关系。

--- src/Models/Answer.php
+++ src/Models/Answer.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Answer extends Model
 {
     /**
-     * Has the answer for the question.
+     * Has the question for answer.
      *
      * @return \Illuminate\Database\Eloquent\Relations\HasOne|null
      * @author Seven Du <shiweidu@outlook.com>
@@ -15,5 +15,16 @@ class Answer extends Model
     public function question()
     {
         return $this->hasOne(Question::class, 'id', 'question_id');
+    }
+
+    /**
+     * Has user for answer.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo|null
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id', 'user_id');
     }
 }
