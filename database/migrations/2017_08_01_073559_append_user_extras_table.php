d9c2e29086bb92c82c25d60a5722f0a71b5ff5db
Seven Du <shiweidu@outlook.com>
2017-08-01 15:38:15 +0800

    用户拓展数据表增加用户问题回答数统计。

--- database/migrations/2017_08_01_073559_append_user_extras_table.php
+++ database/migrations/2017_08_01_073559_append_user_extras_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AppendQuestionsCountUserExtrasTable extends Migration
+class AppendUserExtrasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -16,6 +16,12 @@ class AppendQuestionsCountUserExtrasTable extends Migration
         if (! Schema::hasColumn('user_extras', 'questions_count')) {
             Schema::table('user_extras', function (Blueprint $table) {
                 $table->integer('questions_count')->unsigned()->nullable()->default(0)->comment('用户提问数统计');
+            });
+        }
+
+        if (! Schema::hasColumn('user_extras', 'answers_count')) {
+            Schema::table('user_extras', function (Blueprint $table) {
+                $table->integer('answers_count')->unsigned()->nullable()->default(0)->comment('用户回答数统计');
             });
         }
     }
@@ -30,6 +36,12 @@ class AppendQuestionsCountUserExtrasTable extends Migration
         if (Schema::hasColumn('user_extras', 'questions_count')) {
             Schema::table('user_extras', function (Blueprint $table) {
                 $table->dropColumn('questions_count');
+            });
+        }
+
+        if (Schema::hasColumn('user_extras', 'answers_count')) {
+            Schema::table('user_extras', function (Blueprint $table) {
+                $table->dropColumn('answers_count');
             });
         }
     }
