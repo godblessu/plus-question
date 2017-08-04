f050eaaeb8ffef9aa6f5437574731ec84ee5a22c
Seven Du <shiweidu@outlook.com>
2017-08-04 16:51:02 +0800

    增加答案打赏相关语言包。

--- resource/lang/zh-CN/answers.php
+++ resource/lang/zh-CN/answers.php
@@ -11,4 +11,15 @@ return [
             'body' => '回答问题《:body》',
         ],
     ],
+    'reward' => [
+        'attributes' => [
+            'amount' => '金额',
+        ],
+        'required' => '请输入打赏:attribute',
+        'min' => '你输入的:attribute非法',
+        'max' => '余额不足',
+        'not-user' => '打赏对象不存在，无法进行搭讪。',
+        'send-reward' => '打赏问答答案。',
+        'get-reward' => '问答回答被打赏',
+    ],
 ];
