53cc0cb286536afc47ee7d899e51ed4eaf32b5c2
Seven <shiweidu@outlook.com>
2017-08-09 06:46:30 +0000

    Apply fixes from StyleCI

--- src/Models/QuestionApplication.php
+++ src/Models/QuestionApplication.php
@@ -5,7 +5,7 @@ namespace SlimKit\PlusQuestion\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class QuestionApplication extends Model
-{   
+{
     protected $table = 'question_application';
 
     protected $guarded = ['id', 'created_at', 'updated_at'];
